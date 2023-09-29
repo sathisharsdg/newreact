f21589605a7bc0e25696f846462c1fe2eda6d5a8
Sathish <sathish.333ddd@gmail.com>
2023-09-29 11:39:52 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import About from "./Components/About";
+import Blog from "./Components/Blog";
+
+function App(){
+   return(
+     <div>
+         <About/>
+         <h1>This is app component</h1>
+         <Blog/>
+     </div>
+   )
+}
+
+export default App;
